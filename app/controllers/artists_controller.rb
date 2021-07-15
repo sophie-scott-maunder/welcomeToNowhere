999e3e811ba8f58275d77ccf5204f5dbee4a6a5a
Jack Ewing <jackewing23@gmail.com>
2021-07-15 17:08:44 +1200

    styling

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,28 @@
 class ArtistsController < ApplicationController
+
   def index
-    @artists = Airrecord.table(Rails.application.credentials.airtable_key, 'appQU5xzDT5zzQdtO', 'Artists').all.map do |artist|
+    @artists = artists
+    @colours = colours
+  end
+
+  def show
+    @artists = artists
+    @colours = colours
+    @index = params[:id].to_i
+  end
+
+  private
+
+  def image(record)
+    if record['Portrait Press Shot'].present?
+      if record['Portrait Press Shot'][0]['thumbnails'].present?
+        record['Portrait Press Shot'][0]['thumbnails']['large']['url']
+      end
+    end
+  end
+
+  def artists
+    Airrecord.table(Rails.application.credentials.airtable_key, 'appQU5xzDT5zzQdtO', 'Artists').all.map do |artist|
       {
         name: artist['Performer/Band Name'],
         image_url: image(artist),
@@ -12,13 +34,9 @@ class ArtistsController < ApplicationController
     end
   end
 
-  private
-
-  def image(record)
-    if record['Portrait Press Shot'].present?
-      if record['Portrait Press Shot'][0]['thumbnails'].present?
-        record['Portrait Press Shot'][0]['thumbnails']['large']['url']
-      end
-    end
+  def colours
+    [
+      'rgba(243, 166, 131,1.0)', 'rgba(247, 215, 148,1.0)', 'rgba(119, 139, 235,1.0)', 'rgba(231, 127, 103,1.0)', 'rgba(207, 106, 135,1.0)', 'rgba(120, 111, 166,1.0)', 'rgba(248, 165, 194,1.0)', 'rgba(99, 205, 218,1.0)', 'rgba(234, 134, 133,1.0)', 'rgba(89, 98, 117,1.0)'
+    ]
   end
 end
