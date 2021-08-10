32596373103da3d1e301a9abf6ee2907a5339702
Jack Ewing <jackewing23@gmail.com>
2021-08-10 17:49:01 +1200

    add assets and style nav

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,18 +13,40 @@ Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
 
-import Client from "shopify-buy";
+// import Client from "shopify-buy";
 
-// Initializing a client to return content in the store's primary language
-const client = Client.buildClient({
-  domain: "eyegum.myshopify.com",
-  storefrontAccessToken: "3d8921da235f01fcf0d51ad2fb3997e7",
-});
+// // Initializing a client to return content in the store's primary language
+// const client = Client.buildClient({
+//   domain: "eyegum.myshopify.com",
+//   storefrontAccessToken: "3d8921da235f01fcf0d51ad2fb3997e7",
+// });
 
-// Fetch all products in your shop
-client.product.fetchAll().then((products) => {
-  // Do something with the products
-  a = document.getElementById("product");
-  a.style = "background-image: " + products[0].images.src;
-  products[0].images.src;
+// // Fetch all products in your shop
+// client.product.fetchAll().then((products) => {
+//   // Do something with the products
+//   a = document.getElementById("product");
+//   a.style = "background-image: " + products[0].images.src;
+//   products[0].images.src;
+// });
+
+const speeds = [8, 16, 26, 36, 49, 69];
+
+const aminate_backround = () => {
+  var top = window.pageYOffset;
+
+  var layers = document.getElementsByClassName("parallax");
+  var layer, speed, yPos;
+  for (var i = 0; i < layers.length; i++) {
+    layer = layers[i];
+    speed = speeds[i];
+    var yPos = -((top * speed) / 100);
+    layer.setAttribute(
+      "style",
+      "transform: translate3d(0px, " + yPos + "px, 0px)"
+    );
+  }
+};
+
+window.addEventListener("scroll", () => {
+  window.requestAnimationFrame(aminate_backround);
 });
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -22,7 +22,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
@@ -56,7 +56,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
